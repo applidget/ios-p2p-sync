195f72b44831eb3fd19ae0f7c2b802eb8e22b983
robinmonjo <robin.monjo@applidget.com>
2012-07-24 17:46:56 +0200

    Initial Commit

---
+++ RSConnection/RSConnection.h
@@ -0,0 +1,13 @@
+//
+//  RSConnection.h
+//  RSConnection
+//
+//  Created by Robin on 24/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSConnection : NSObject
+
+@end
---
+++ RSConnection/RSConnection.m
@@ -0,0 +1,13 @@
+//
+//  RSConnection.m
+//  RSConnection
+//
+//  Created by Robin on 24/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RSConnection.h"
+
+@implementation RSConnection
+
+@end
